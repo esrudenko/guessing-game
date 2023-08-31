0c729b7eb4a23ed592f6acbb5122f1d49046ace5
Elena Mankerova <esrudenko93@gmail.com>
2023-08-31 17:27:55 +0600

    task done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.minNumber = min;
+        this.maxNumber = max;
+        this.guessNumber = 0;
     }
 
     guess() {
-
+        return this.guessNumber = Math.ceil((this.minNumber + this.maxNumber) / 2);
     }
 
     lower() {
-
+        this.maxNumber = this.guessNumber;
     }
 
     greater() {
-
+        this.minNumber = this.guessNumber;
     }
 }
 
